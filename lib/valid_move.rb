7ef0da7de66474f9f26c911ebff79df78b2541c4
Cathy D'Onofrio <catd825@gmail.com>
2020-03-11 00:30:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board,index)
-  index.between(0,8) && !position_taken?(board,index)
+  index.between?(0,8) && !position_taken?(board,index)
   #
   #if board[index] != " " && board[index].between?(0,8)
   #   return true
